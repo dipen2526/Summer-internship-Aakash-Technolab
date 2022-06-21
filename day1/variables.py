991f70685ddd24c90f6f846708fd535da7b30cad
Dipen Panchal <100028854+dipen2526@users.noreply.github.com>
2022-06-21 09:35:04 +0530

    Update variables.py

--- day1/variables.py
+++ day1/variables.py
@@ -1,1 +1,29 @@
+# variable declaration
+a=10
+b=10.5
+c="akash technolabs"
 
+print(a)
+print("value of b is : ",b)
+print("company name is : ",c)
+
+#changing the value of a variable
+name="Akash Technolabs"
+print("Name is : ",name)
+
+#assigning a new value to website
+name="akashsir.com"
+print("Name is : ",name)
+
+#assigning multiple values to multiple variables
+a,b,c = 10,10.5,"Akash technolabs"
+print(a)
+print("value of b is : ",b)
+print("company name is : ",c)
+
+#assign the same value to multiple variables
+a=b=c=10
+
+print("value of a is : ",a)
+print("value of b is : ",b)
+print("value of c is : ",c)
