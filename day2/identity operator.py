a875d2a9271f1fd6c85cc74508ccc3153123884a
Dipen Panchal <100028854+dipen2526@users.noreply.github.com>
2022-06-21 11:12:37 +0530

    Add files via upload

---
+++ day2/identity operator.py
@@ -0,0 +1,7 @@
+#identity operator
+
+x=10
+y=10
+
+print(x is y)
+print(x is not y)
---
+++ day2/membership operator.py
@@ -0,0 +1,9 @@
+#membership operator
+
+x=10
+y=5
+list1 = [10,15,20,25,30]
+
+print(x in list1)
+print(y in list1)
+print(y not in list1)
