20fefe088ad3683a2d24f862ab5c54d10f199e33
Dipen Panchal <100028854+dipen2526@users.noreply.github.com>
2022-06-21 09:36:09 +0530

    Add files via upload

---
+++ day1/numberdatatype.py
@@ -0,0 +1,10 @@
+#number datatype
+n1=10
+print(n1,"is of type",type(n1))
+
+n2=10.5
+print(n2,"is of type",type(n2))
+print(n2,"is complex number?",isinstance(10.5,int))
+
+n3=1+2j
+print(n3,"is complex number?",isinstance(1+2j,complex))
