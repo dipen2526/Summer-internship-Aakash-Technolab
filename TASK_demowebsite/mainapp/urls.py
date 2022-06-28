82db445f51faee2dda934f6261c142f09155b473
Dipen Panchal <100028854+dipen2526@users.noreply.github.com>
2022-06-28 17:06:22 +0530

    Add files via upload

---
+++ TASK_demowebsite/mainapp/urls.py
@@ -0,0 +1,9 @@
+from importlib.resources import path
+from django.urls import URLPattern,path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact')
+]
