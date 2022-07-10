128ec37584a25464de625eb45d6a85232a23a76d
Dipen Panchal <100028854+dipen2526@users.noreply.github.com>
2022-07-10 17:17:16 +0530

    Delete admin.py

--- Onlinefire_reportingsystem/firereport/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Firereport)
-admin.site.register(Firetequesthistory)
-admin.site.register(Teams)
